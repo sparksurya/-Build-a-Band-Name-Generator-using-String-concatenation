2381e2630031881cf66d422e6f65ea96795e94c5
Suryajeyanth <80058535+sparksurya@users.noreply.github.com>
2022-08-06 12:54:31 +0530

    Add files via upload

---
+++ Band Name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band  Name Genearator")
+a= input("What name of The City You Grow up in ? \n")
+b= input ("What is Your Band  Name ?\n ")
+print("Your Band Name is " + (a)  + " " + (b))
